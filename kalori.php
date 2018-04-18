e418e1371aee4d52866f2f000f569c7035dcbf46
Muhammad Ihsan <ihsanmhd01@gmail.com>
2018-04-18 14:36:01 +0700

    Update kalori.php

--- kalori.php
+++ kalori.php
@@ -38,9 +38,6 @@
     padding: 15px;
     margin: auto;
   }
-  .form-signin .checkbox {
-    font-weight: 400;
-  }
   .form-signin .form-control {
     position: relative;
     box-sizing: border-box;
@@ -51,15 +48,10 @@
   .form-signin .form-control:focus {
     z-index: 2;
   }
-  .form-signin input[type="email"] {
+  .form-signin input[type="text"] {
     margin-bottom: -1px;
     border-bottom-right-radius: 0;
     border-bottom-left-radius: 0;
-  }
-  .form-signin input[type="password"] {
-    margin-bottom: 10px;
-    border-top-left-radius: 0;
-    border-top-right-radius: 0;
   }
 </style>
 </head>
